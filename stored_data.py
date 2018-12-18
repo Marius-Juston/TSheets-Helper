9af78f24a3f3e60586f3d462c1eb0032535d55f4
NamelessSuperCoder <Marius.juston@hotmail.fr>
2018-12-18 18:32:03 -0500

    added creation of timesheets and jobcodes tables

--- stored_data.py
+++ stored_data.py
@@ -18,9 +18,15 @@ class TSheetsCache:
         if self.table_exists('users'):
             self.cursor.execute("CREATE TABLE users (id INTEGER, name text, email text)")
             self.conn.commit()
+
     def create_timesheets_table(self):
-        if self.table_exists('users'):
-            self.cursor.execute("CREATE TABLE users (id INTEGER, name text, email text)")
+        if self.table_exists('timesheets'):
+            self.cursor.execute("CREATE TABLE timesheets (id INTEGER, date DATE, duration INTEGER, jobcode_id INTEGER)")
+            self.conn.commit()
+
+    def create_jobcodes_table(self):
+        if self.table_exists('jobcodes'):
+            self.cursor.execute("CREATE TABLE jobcodes (id INTEGER, parent_id INTEGER,name TEXT)")
             self.conn.commit()
 
     def close(self):
