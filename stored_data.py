431e27e923055b52a886a6228f3b2c11a7aa1fdf
NamelessSuperCoder <Marius.juston@hotmail.fr>
2018-12-19 17:47:54 -0500

    made the table names as class variables

--- stored_data.py
+++ stored_data.py
@@ -2,7 +2,13 @@ import sqlite3
 
 
 class TSheetsCache:
-    def __init__(self, database_file="tsheets_info.db") -> None:
+    users_table = 'users'
+    jobcodes_table = 'jobcodes'
+    timesheets_table = 'timesheets'
+    time_stamp_table = 'info_timestamp'
+
+
+    def __init__(self, database_file="tsheets_info.db", update_rates: dict = None) -> None:
         super().__init__()
         self.conn = sqlite3.connect(database_file)
         self.cursor = self.conn.cursor()
@@ -15,18 +21,19 @@ class TSheetsCache:
             return False
 
     def create_username_table(self):
-        if self.table_exists('users'):
-            self.cursor.execute("CREATE TABLE users (id INTEGER, name text, email text)")
+        if not self.table_exists(self.users_table):
+            self.cursor.execute("CREATE TABLE users (id INTEGER PRIMARY KEY, name text, email text) ")
             self.conn.commit()
 
     def create_timesheets_table(self):
-        if self.table_exists('timesheets'):
-            self.cursor.execute("CREATE TABLE timesheets (id INTEGER, date DATE, duration INTEGER, jobcode_id INTEGER)")
+        if not self.table_exists(self.timesheets_table):
+            self.cursor.execute(
+                "CREATE TABLE timesheets (id INTEGER PRIMARY KEY, date DATE, duration INTEGER, jobcode_id INTEGER)")
             self.conn.commit()
 
     def create_jobcodes_table(self):
-        if self.table_exists('jobcodes'):
-            self.cursor.execute("CREATE TABLE jobcodes (id INTEGER, parent_id INTEGER,name TEXT)")
+        if not self.table_exists(self.jobcodes_table):
+            self.cursor.execute("CREATE TABLE jobcodes (id INTEGER PRIMARY KEY, parent_id INTEGER,name TEXT)")
             self.conn.commit()
 
     def create_timestamp_table(self):
